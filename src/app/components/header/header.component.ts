3fcbdb5ab887b03f1d188d4c1bc90ec730e85aeb
HANCQ <duncan.hancq@disney.com>
2019-06-19 13:46:18 +0200

    update carroussel v0.1

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ){}
 
+  goTo(page){
+    this.router.navigateByUrl(page);
+  }
+  
   ngOnInit() {}
 
 }
